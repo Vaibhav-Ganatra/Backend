39f213163630561343b89d4dc4fdce051b4d7153
Vaibhav Ganatra <vaib2641@gmail.com>
2020-06-21 17:57:30 +0530

    Port fixed

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ mongoose.connect(process.env.MONGO_URL,{
 //Creating Server
 const http= require("http");
 const server= http.createServer(app);
-const port= process.env.port||3000;
+const port= process.env.PORT||3000;
 server.listen(port,(err) =>{
    if(err)
       console.log(err);
